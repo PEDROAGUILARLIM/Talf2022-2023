adb5a1610aee1a8b768047319955982606b59a2a
Pedro-a4 <37327234+Pedro-a4@users.noreply.github.com>
2022-12-24 21:01:00 +0100

    Add files via upload

---
+++ Práctica 4/printNvectors.m
@@ -0,0 +1,9 @@
+function printNvectors(N)
+
+  for i=0:N-1
+    
+    disp([ ' ( ' num2str(godeldecoding(i)) ' ) ' ])
+  
+  end
+  
+end
---
+++ Práctica 4/printNwhilePrograms.m
@@ -0,0 +1,9 @@
+function printNwhilePrograms(N)
+
+  for i=0:N-1
+  
+    disp(N2WHILE(i))
+    
+  end
+  
+ end
